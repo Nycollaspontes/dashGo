cf21006dc6bb4c661429b471c74d17a71791ec55
Nycollas Pontes <nycollaspontes@gmail.com>
2022-06-28 03:59:50 -0300

    Coding Updates

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,16 +1,23 @@
-import { Text, Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import { Text, Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, useBreakpointValue, Spinner } from "@chakra-ui/react";
 import Link from "next/link";
+
 import { RiAddLine } from "react-icons/ri";
 import { Header } from "../../components/header/Index";
 import { Pagination } from "../../components/pagination/Index";
 import { Sidebar } from "../../components/sidebar/Index";
+import { useUsers } from "../../services/hooks/useUsers";
+
+
 
 
 export default function UserList() {
+    const { data, isLoading,isFetching, error } = useUsers();
+
     const wideVersion = useBreakpointValue({
         base: false,
         lg: true,
     })
+
 
     return (
         <Box>
@@ -21,7 +28,11 @@ export default function UserList() {
 
                 <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
                     <Flex mb='8' justify='space-between' align='center' >
-                        <Heading size='lg' fontWeight='normal' >Usuários </Heading>
+                        <Heading size='lg' fontWeight='normal' >
+                            Usuários
+
+                            {! isLoading && isFetching && <Spinner size='sm' color='gray.500' className='ml-4'/>}
+                        </Heading>
                         <Link href='/users/Create' passHref>
                             <Button as='a'
                                 size='md'
@@ -34,36 +45,56 @@ export default function UserList() {
                         </Link>
                     </Flex>
 
-                    <Table colorScheme='whiteAlpha'>
-                        <Thead>
-                            <Tr>
-                                <Th px={['4', '4', '6']} color='gray.300' width='8'>
-                                    <Checkbox colorScheme='pink' />
-                                </Th>
-                                <Th> Usuário</Th>
-                                {wideVersion && <Th>Data de cadastro</Th>}
-                                <Th width='8'></Th>
-                            </Tr>
-                        </Thead>
+                    {isLoading ? (
+                        <Flex justify='center'>
+                            <Spinner />
+                        </Flex>
+                    )
+                        : error ? (
+                            <Flex>
+                                <Text>Falha ao obter dados dos usuarios.</Text>
+                            </Flex>
+                        )
+                            : (
+                                <>
+                                    <Table colorScheme='whiteAlpha'>
+                                        <Thead>
+                                            <Tr>
+                                                <Th px={['4', '4', '6']} color='gray.300' width='8'>
+                                                    <Checkbox colorScheme='pink' />
+                                                </Th>
+                                                <Th> Usuário</Th>
+                                                {wideVersion && <Th>Data de cadastro</Th>}
+                                                <Th width='8'></Th>
+                                            </Tr>
+                                        </Thead>
 
-                        <Tbody>
-                            <Tr>
-                                <Td px='6'>
-                                    <Checkbox colorScheme='pink' />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight='bold'>Nycollas Pontes</Text>
-                                        <Text fontSize='sm' color='gray.300'>nycollaspontes@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {wideVersion && <Td>04 de abril ,2021</Td>}
-                            </Tr>
-                        </Tbody>
+                                        <Tbody>
+                                            {data.map((user: { id: string; name: string; email: string; createdAt: string; }) => {
+                                                return (
+                                                    <Tr key={user.id}>
 
-                    </Table>
+                                                        <Td px='6'>
+                                                            <Checkbox colorScheme='pink' />
+                                                        </Td>
+                                                        <Td>
+                                                            <Box>
+                                                                <Text fontWeight='bold'>{user.name}</Text>
+                                                                <Text fontSize='sm' color='gray.300'>{user.email}</Text>
+                                                            </Box>
+                                                        </Td>
+                                                        {wideVersion && <Td>{user.createdAt}</Td>}
+                                                    </Tr>
+                                                )
+                                            })}
+                                        </Tbody>
 
-                    <Pagination />
+                                    </Table>
+
+                                    <Pagination />
+                                </>
+                            )}
+
                 </Box>
             </ Flex >
         </Box>
